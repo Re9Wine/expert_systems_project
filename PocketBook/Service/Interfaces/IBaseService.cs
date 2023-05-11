fd25cce3cf81711185da4d30b403c8759972471b
Re9Wine <asckortan@gmail.com>
2023-05-11 21:32:38 +0300

    Create service interfaces

---
+++ PocketBook/Service/Interfaces/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interfaces
+{
+    public interface IBaseService<T>
+    {
+        Task<bool> Create(T entity);
+        Task<bool> Update(T entity);
+        Task<bool> DeleteById(Guid id);
+        Task<T?> GetById(Guid id);
+        Task<List<T>> GetAll();
+    }
+}
