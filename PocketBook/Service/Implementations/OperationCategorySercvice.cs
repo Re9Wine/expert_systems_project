d144e3d31f3a0e541e414fa5e154b6ba17a5f023
Re9Wine <asckortan@gmail.com>
2023-05-24 21:50:58 +0300

    Created new services and implemented their

---
+++ PocketBook/Service/Implementations/OperationCategorySercvice.cs
@@ -0,0 +1,58 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Service.Interfaces;
+
+namespace Service.Implementations
+{
+    public class OperationCategorySercvice : IOperationCategorySercvice
+    {
+        private readonly IOperationCategoryRepository _repository;
+
+        public OperationCategorySercvice(IOperationCategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<bool> Create(OperationCategory operationCategory)
+        {
+            if (operationCategory == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            return _repository.Create(operationCategory);
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            var operationCategory = await _repository.GetById(id);
+
+            if (operationCategory == null)
+            {
+                return false;
+            }
+
+            return await _repository.Delete(operationCategory);
+        }
+
+        public Task<List<OperationCategory>> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Task<OperationCategory?> GetById(Guid id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public Task<bool> Update(OperationCategory operationCategory)
+        {
+            if (operationCategory == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            return _repository.Update(operationCategory);
+        }
+    }
+}
---
+++ PocketBook/Service/Interfaces/IOperationWithMoneyService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entity;
+
+namespace Service.Interfaces
+{
+    public interface IOperationWithMoneyService
+    {
+        Task<OperationWithMoney?> GetById(Guid id);
+        Task<List<OperationWithMoney>> GetAll();
+        Task<bool> Create(OperationWithMoney operationWithMoney);
+        Task<bool> Update(OperationWithMoney operationWithMoney);
+        Task<bool> Delete(Guid id);
+        Task<List<OperationWithMoney>> GetFiveLatestConsumption();
+    }
+}
