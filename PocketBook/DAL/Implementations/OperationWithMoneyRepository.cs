29bd7f21da1aa3fee5e5062fa464915881f55acc
Re9Wine <asckortan@gmail.com>
2023-05-24 21:49:43 +0300

    Create new repository interfaces and implemented their

---
+++ PocketBook/DAL/Implementations/OperationWithMoneyRepository.cs
@@ -0,0 +1,67 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Implementations
+{
+    public class OperationWithMoneyRepository : IOperationWithMoneyRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OperationWithMoneyRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<bool> Create(OperationWithMoney entity)
+        {
+            if (entity == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _context.OperationWithMoneys.Add(entity);
+
+            return Task.FromResult(_context.SaveChangesAsync().Result != 0);
+        }
+
+        public Task<bool> Delete(OperationWithMoney entity)
+        {
+            if (entity == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _context.OperationWithMoneys.Remove(entity);
+
+            return Task.FromResult(_context.SaveChangesAsync().Result != 0);
+        }
+
+        public Task<List<OperationWithMoney>> GetAll()
+        {
+            return _context.OperationWithMoneys.ToListAsync();
+        }
+
+        public Task<OperationWithMoney?> GetById(Guid id)
+        {
+            return _context.OperationWithMoneys.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<List<OperationWithMoney>> GetFiveLatestConsumption()
+        {
+            return _context.OperationWithMoneys.OrderBy(x => x.Date).Where(x => x.Value < 0).Take(5).ToListAsync();
+        }
+
+        public Task<bool> Update(OperationWithMoney entity)
+        {
+            if (entity == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _context.OperationWithMoneys.Update(entity);
+
+            return Task.FromResult(_context.SaveChangesAsync().Result != 0);
+        }
+    }
+}
---
+++ PocketBook/DAL/Interfaces/IOperationCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entity;
+
+namespace DAL.Interfaces
+{
+    public interface IOperationCategoryRepository : IBaseRepository<OperationCategory>
+    {
+        Task<List<OperationCategory>> GetByType(string type);
+    }
+}
