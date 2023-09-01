504293722ad15a8403354801704731122c048f7c
Re9Wine <asckortan@gmail.com>
2023-09-01 14:40:34 +0300

    add: create view entities

---
+++ PocketBook/Domain/ViewEntity/OperationCategoryView.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.ViewEntity
+{
+    public class OperationCategoryView
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Outlay { get; set; } = decimal.Zero;
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+        public OperationCategoryView() { }
+
+        public OperationCategoryView(string name, decimal outlay)
+        {
+            Name = name;
+            Outlay = outlay;
+        }
+
+        public OperationCategoryView(DateTime date, decimal outlay)
+        {
+            Outlay = outlay;
+            Date = date;
+        }
+    }
+}
---
+++ PocketBook/Domain/ViewEntity/OperationWithMoneyView.cs
@@ -0,0 +1,26 @@
+﻿using Domain.DatabaseEntity;
+
+namespace Domain.ViewEntity
+{
+    public class OperationWithMoneyView
+    {
+        public Guid Id { get; set; }
+        public bool IsConsumption { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public decimal Value { get; set; }
+
+        public OperationWithMoneyView() { }
+
+        public OperationWithMoneyView(OperationWithMoney operationWithMoney)
+        {
+            Id = operationWithMoney.Id;
+            IsConsumption = operationWithMoney.OperationCategoryNavigation.IsConsumption;
+            CategoryName = operationWithMoney.OperationCategoryNavigation.Name;
+            Date = operationWithMoney.Date;
+            Description = operationWithMoney.Description;
+            Value = operationWithMoney.Value;
+        }
+    }
+}
